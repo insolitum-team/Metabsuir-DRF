d4c50e4e2c10a0d0d518043f5a4c0a8123005659
ImmortalDante <maklakovfdr@gmail.com>
2022-09-23 10:32:14 +0300

    function-based views added

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 from .models import Section, Theme, Comment
 from .serializers import SectionSerializer, ThemeSerializer, CommentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 class SectionViewSet(viewsets.ModelViewSet):
@@ -16,3 +18,17 @@ class ThemeViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+@api_view(['GET'])
+def get_themes(request, section_id):
+    themes = Theme.objects.filter(section_id=section_id).all()
+    serializer = ThemeSerializer(themes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_comments(request, theme_id):
+    comments = Comment.objects.filter(theme_id=theme_id).all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
